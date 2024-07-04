278d9fb9a522ffb3131f4a17853961ef2483e639
Shashwat Kamboj <57861101+kambojshashwat@users.noreply.github.com>
2024-07-05 01:35:10 +0530

    Create Jenkins.sh
    This Script will install Jenkins in UBUNTU OS

---
+++ Jenkins.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt install git openjdk-8-jdk maven -y
+sudo apt install openjdk-11-jdk -y
+
+sudo update-alternatives --config java
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt install jenkins
+systemctl start jenkins
