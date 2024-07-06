6cff6ebb5882ad74d3f8ba84a1ea923f939081a2
Shashwat Kamboj <57861101+kambojshashwat@users.noreply.github.com>
2024-07-06 22:40:49 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,7 +1,7 @@
 #AMAZON LINUX 2023 TOMCAT INSTALL SHELL SCRIPT
 
 sudo yum install java-1.8.0-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.90/bin/apache-tomcat-9.0.90.tar.gz
 tar -zxvf apache-tomcat-9.0.89.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.89/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.89/conf/tomcat-users.xml
